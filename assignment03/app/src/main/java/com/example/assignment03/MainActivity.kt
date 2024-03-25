5eb18eb595be8d064ae3fac5b2f87e837643d190
VatsalBoricha <113151273+VatsalBoricha@users.noreply.github.com>
2024-03-24 22:36:42 -0400

    updated main Activity

--- assignment03/app/src/main/java/com/example/assignment03/MainActivity.kt
+++ assignment03/app/src/main/java/com/example/assignment03/MainActivity.kt
@@ -3,6 +3,14 @@ package com.example.assignment03
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+* Assignment 3 - To Do List App
+* Name : Vatsal Boricha
+* StudentID: 200528698
+* Date: 24th march 2024
+* Description: This is a To Do List app that can add tasks, delete or edit them.
+* It can also be viewed in calendar view. The toggle switch is to indicate if the task is completed or not.There is also input
+* text for the name of tasks and notes. There is optional due dates and completed toggle switches.  */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
